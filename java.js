f9a92d1b8f40552563219640c1dad0028746c3eb
Zarel Last <lastzarel@gmail.com>
2019-01-07 22:56:24 +0700

    test

--- java.js
+++ java.js
@@ -7,7 +7,7 @@ const nomorDOM = document.getElementById('nomor');
 const data = JSON.parse(hardisk.getItem('todos'));
 
 console.log(data);
-if(data == null||data.length <= 0){
+if(data.length == null||data.length <= 0){
 	todos = todos;
 }else{
 	todos = data;
