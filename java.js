23732bd3836820cbae3e148fe19fc749643a66bb
Zarel Last <lastzarel@gmail.com>
2019-01-07 23:40:34 +0700

    test

--- java.js
+++ java.js
@@ -46,7 +46,7 @@ addDOM.addEventListener('click',function(){
 	console.log(todos);
 	hardisk.setItem('todos',JSON.stringify(todos));
 	nomorDOM.innerHTML = todos.length+" To Do";
-	if(todos.length <= 0){
+	if(data==null){
 	document.getElementById("starting").style.display = "block";
 	}else{
 	todos = data;
