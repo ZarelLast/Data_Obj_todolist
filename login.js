a0ec7b81f78aabf5c3d58da0cb1e2853a56d0eba
Zarel Last <lastzarel@gmail.com>
2019-01-08 09:51:24 +0700

    test

--- login.js
+++ login.js
@@ -1,1 +1,9 @@
-const initext = prompt('hallo');
+const nama = prompt("Masukan Username : ");
+const user = document.getElementById("utama");
+	if(nama = "Zarel"){
+		user.innerHTML = user.innerHTML "Hallo" + username;
+	}else{
+		user.innerHTML =user.innerHTML "oops" + username;
+	};
+	
+
